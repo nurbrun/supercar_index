b1acb78ca4c31ad2ad8e0a737bbe8466180440fc
Nurbrun <bruno.alfano.rizzo@gmail.com>
2015-01-13 14:21:55 -0500

    added 9 supercar attributes and ran a migration (included intial sorcery user migration)

--- supercarindex2.0/db/migrate/20150113184811_create_supercars.rb
+++ supercarindex2.0/db/migrate/20150113184811_create_supercars.rb
@@ -1,8 +1,17 @@
 class CreateSupercars < ActiveRecord::Migration
   def change
     create_table :supercars do |t|
+    	t.string :manufacturer
+    	t.string :model
+    	t.string :color
+    	t.string :city
+    	t.string :country
+    	t.string :spot_date
+    	t.string :spot_type
+    	t.string :car_class
+    	t.string :photo_url
 
-      t.timestamps
+      t.timestamp
     end
   end
 end
