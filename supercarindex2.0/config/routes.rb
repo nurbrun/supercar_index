11cca15b08482a1e008479aa42bbaf4d1f4e3f9c
Nurbrun <bruno.alfano.rizzo@gmail.com>
2015-01-14 13:44:12 -0500

    added sessions controller with create and destroy actions. created resources routes for sessions and supercars.

--- supercarindex2.0/config/routes.rb
+++ supercarindex2.0/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :supercars
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
